33cff546abc099bbab659e018b32c4bd7f808e6e
Dereck Hidalgo <dereckhidalgo27@gmail.com>
2021-10-29 23:28:17 -0400

    Horarios y links

--- bot.js
+++ bot.js
@@ -10,7 +10,6 @@ const bot = new Telegraf(Token);
 
 //Cuando el bot inicie, lance mensaje de bienvenido
 bot.start((ctx)=>{
-    console.log(ctx.chatMember);
     if(ctx.from.first_name =="Lisandro"){
         ctx.reply("Lisandro va de ahí mmñ");
     }else if(ctx.from.first_name =="Dereck"){
@@ -23,12 +22,12 @@ bot.start((ctx)=>{
 
 //Comando help
 bot.help((ctx)=>{
-    ctx.reply(`Los comandos habilitados por el momento son:
-    1. Llamar a todos: 
-    /everyone, /all, /venganto, /toElMundazo, /lista, /toelmundo.
-    2. Horario:
-    /Clases, /Horario, /clasesNormales. 
-    `);
+    ctx.reply(`Los comandos habilitados por el momento son: 
+1. Llamar a todos: 
+/everyone, /all, /venganto, /toElMundazo, /lista, /toelmundo.
+2. Horario:
+/Clases, /Horario, /clasesNormales. 
+`);
 })
 
 
@@ -47,6 +46,26 @@ bot.command(['clasesNormales','clases', 'CLASES', 'Clases', 'Horario','horario',
     `)
 });
 
+bot.command("DiseloTuBot", ctx=>{
+    ctx.reply("Te la bebiste Eduardo");
+})
+bot.command(["linkParalela","LinkParalela","linkparalela","LINKPARALELA","Linkparalela"], ctx=>{
+    ctx.reply("El link de la clase programación paralela es: https://meet.google.com/gcs-jwgg-tch?authuser=0");
+})
+bot.command(["linkIngenieria","LinkIngenieria","linkingenieria","LINKINGENIERIA","linkEvanyeline","LinkEvanyeline","linkevanyeline","LINKEVANYELINE","linkevangelion","linkEvangelion","LinkEvangelion"], ctx=>{
+    ctx.reply("El link de la clase Ingeniería de Software es: https://meet.google.com/asr-jzyw-hmn");
+})
+
+
+bot.command("links", ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id, 'Estos son los diferentes links para sus clases: \n ¿Cuál desea obtener?',{
+        reply_markup: {
+            inline_keyboard: [
+                [{text: "Programación Paralela", callback_data: "linkParalela"},{text: "Ingeniería de Software", callback_data: "linkingenieria"}]
+            ]
+        }
+    })
+})
 
 //Eventos con on
 // bot.on('text',ctx=>{
